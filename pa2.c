57c0ba13f8bd611044e5753033f5f3c316d3bb4b
jisushin0313 <jisushin0313@github.com>
2020-07-05 23:01:20 +0900

    OS_PA2

--- pa2.c
+++ pa2.c
@@ -62,6 +62,37 @@ extern unsigned int mapcounts[];
  */
 unsigned int alloc_page(unsigned int vpn, unsigned int rw)
 {
+	// 
+	unsigned int pd_index = vpn/NR_PTES_PER_PAGE;
+	unsigned int pte_index = vpn%NR_PTES_PER_PAGE;
+	
+	// is page table valid?
+	// page table doesn't exist!
+	if(current->pagetable.outer_ptes[pd_index] == NULL){
+		struct pte_directory *new_pd = malloc(sizeof(struct pte_directory));
+		current->pagetable.outer_ptes[pd_index]=new_pd;
+		//printf("current page table is empty. fill the page with next level page table(16 ptes).\n");
+	}
+	
+	int i=0;
+	for(i=0; i<NR_PAGEFRAMES; i++){
+		// find the empty page frame of smallest #
+		if(mapcounts[i]==0){
+			// mapping vpn-pfn
+			mapcounts[i]++;
+
+			// record on pte
+			current->pagetable.outer_ptes[pd_index]->ptes[pte_index].valid = true;
+			if(rw==(RW_READ|RW_WRITE))
+				current->pagetable.outer_ptes[pd_index]->ptes[pte_index].writable = true;
+			else
+				current->pagetable.outer_ptes[pd_index]->ptes[pte_index].writable = false;
+			current->pagetable.outer_ptes[pd_index]->ptes[pte_index].pfn = i;
+			current->pagetable.outer_ptes[pd_index]->ptes[pte_index].private = rw;
+
+			return i;
+		}
+	}	
 	return -1;
 }
 
@@ -77,8 +108,38 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw)
  */
 void free_page(unsigned int vpn)
 {
-}
+	unsigned int pd_index = vpn/NR_PTES_PER_PAGE;
+	unsigned int pte_index = vpn%NR_PTES_PER_PAGE;
 
+	unsigned int pfn = current->pagetable.outer_ptes[pd_index]->ptes[pte_index].pfn;
+	int process_cnt = mapcounts[pfn];	// the number of processes that refer to page frame #pfn
+	
+	// nothing to free
+	if(process_cnt==0){
+		return;
+	}
+	// something to free
+	else{
+		mapcounts[pfn]--;
+		
+		current->pagetable.outer_ptes[pd_index]->ptes[pte_index].valid=false;
+		current->pagetable.outer_ptes[pd_index]->ptes[pte_index].writable=false;
+		current->pagetable.outer_ptes[pd_index]->ptes[pte_index].pfn=0;
+		current->pagetable.outer_ptes[pd_index]->ptes[pte_index].private=0;
+	}
+
+	// if second-level page table is empty, then free that table
+	int i;
+	// find valid pte
+	for(i=0; i<NR_PTES_PER_PAGE && !(current->pagetable.outer_ptes[pd_index]->ptes[i].valid); i++);
+	// all ptes are invalid, then free that page table
+	if(i==NR_PTES_PER_PAGE){
+		struct pte_directory *p_pte_dir = current->pagetable.outer_ptes[pd_index];
+		current->pagetable.outer_ptes[pd_index]=NULL;
+		free(p_pte_dir);
+	}
+	
+}
 
 /**
  * handle_page_fault()
@@ -98,6 +159,67 @@ void free_page(unsigned int vpn)
  */
 bool handle_page_fault(unsigned int vpn, unsigned int rw)
 {
+	int pd_index = vpn / NR_PTES_PER_PAGE;
+	int pte_index = vpn % NR_PTES_PER_PAGE;
+
+	// if	1. page table is invalid
+	//		2. page directory is invalid
+	//		3. pte is invalid
+	// then, return false
+	struct pagetable *pt=ptbr;
+	struct pte_directory *pd;
+	struct pte *pte;
+
+	// case 1
+	if(!pt)
+		return false;
+
+	pd=pt->outer_ptes[pd_index];
+	
+	// case 2
+	if(!pd)
+		return false;
+
+	pte = &pt->outer_ptes[pd_index]->ptes[pte_index];
+	// case 3
+	if(!pte->valid)
+		return false;
+
+	// if pte is valid,
+	// then check	1. whether the original access mode (private) of the pte is r or rw
+	//				2. if original access mode is rw, then consider copy on write policy
+	//					change pfn and pte information(access mode and pfn...)
+	
+	// case 1 - originally only readable
+	if(pte->private == RW_READ)
+		return false;
+	
+	// case 2
+	int pfn = pte->pfn;
+	// check the # of processes that refer to PA(mapcounts)
+	int number_of_process = mapcounts[pfn];
+	
+	// only one process refer to PA[pfn]
+	if(number_of_process==1){
+		pte->writable=true;
+		return true;
+	}
+	// many
+	else if(number_of_process>1) {
+		// new allocation
+		int i;
+		for(i=0; i<NR_PAGEFRAMES; i++){
+			if(mapcounts[i]==0){
+				mapcounts[i]++;
+
+				pte->writable = true;
+				pte->pfn = i;
+				
+				return true;
+			}
+		}
+	}
+
 	return false;
 }
 
@@ -123,16 +245,65 @@ void switch_process(unsigned int pid)
 	/** YOU CAN CHANGE EVERYTING (INCLUDING THE EXAMPLE) IN THIS FUNCTION **/
 
 	struct process *p;
-
+	int flag = 0;
 	/* This example shows to iterate @processes to find a process with @pid */
 	list_for_each_entry(p, &processes, list) {
+		//if(processes==p->list && flag>0){
+		//	break;
+		//}
+		// 만약 찾는 게 존재하지 않는다면 무한루프 돌 듯. 그러니 이따가 break 하는 거 추가하기
 		if (p->pid == pid) {
 			/* FOUND */
-			break;
+			// chage current process and processes list head
+			current = p;
+			ptbr = &p->pagetable;
+			processes.next = p->list.next;
+			processes.prev = p->list.prev;
+			return;
 		}
 	}
 
+	// the process that i want doesn't exist
+	// fork
 	p = malloc(sizeof(*p));		/* This example shows to create a process, */
+	
+	p->pid = pid;
+	
+	int i, j;
+	struct pagetable *old_pt;
+	struct pte_directory *old_pd;
+	struct pte *old_pte;
+	
+	old_pt=&current->pagetable;
+	struct pagetable *newpt = &p->pagetable;
+
+	for(i=0; i<NR_PTES_PER_PAGE; i++){
+		old_pd = old_pt->outer_ptes[i];
+		if(old_pd != NULL){
+			struct pte_directory *newpd = malloc(sizeof(struct pte_directory));
+			newpt->outer_ptes[i] = newpd;
+
+			for(j=0; j<NR_PTES_PER_PAGE; j++){
+				old_pte = &old_pd->ptes[j];
+				if(old_pte->valid){
+					newpd->ptes[j].valid = true;
+					
+					old_pte->writable=false;
+					newpd->ptes[j].writable=false;
+					
+					newpd->ptes[j].pfn = old_pte->pfn;
+
+					newpd->ptes[j].private = old_pte->private;
+				}
+			}
+
+		}
+
+	}
+
+
+	current = p;
+	ptbr = &p->pagetable;
 	INIT_LIST_HEAD(&p->list);	/* initialize list_head, */
 	list_add_tail(&p->list, &processes);
 								/* and add it to the @processes list */
